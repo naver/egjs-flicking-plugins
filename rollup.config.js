c6e429a26067baf695ce03bcfb583276e7d61563
Daybrush <younkue.choi@navercorp.com>
2019-05-23 18:58:29 +0900

    chore(release): Release 3.1.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,33 @@
+const buildHelper = require("@egjs/build-helper");
+const external = {
+	"@egjs/flicking": "eg.Flicking",
+}
+const name = "eg.Flicking.plugins";
 
-// flicking.js
-// flicking.min.js
-// flicking.pkgd.js
-// flicking.pkgd.min.js
-// flicking.esm.js
+export default buildHelper([
+		{
+      name,
+			input: "./src/index.ts",
+      output: "./dist/plugins.js",
+      format: "umd",
+			external,
+			exports: "named",
+    },
+    {
+      name,
+			input: "./src/index.ts",
+      output: "./dist/plugins.min.js",
+      format: "umd",
+      uglify: true,
+			external,
+			exports: "named",
+    },
+    {
+      input: "./src/index.ts",
+      output: "./dist/plugins.esm.js",
+      format: "esm",
+      external,
+      exports: "named",
+    },
+]);
 
-
-const {esm, umds} = require("./config/bundler");
-
-
-export default [
-  esm({
-		input: "./src/index.ts",
-		output: "./dist/plugins.esm.js",
-  }),
-  ...umds({
-		input: "./src/index.ts",
-		outputs: [
-			`./dist/plugins.js`,
-			`./dist/plugins.min.js`,
-		],
-		exports: "named",
-		library: "eg.Flicking.plugins",
-	}),
-];
