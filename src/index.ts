2e3bb1c080db549acdc877273d354a763850fbf2
seonghyun.ahn <seonghyun.ahn@navercorp.com>
2021-07-09 13:58:42 +0900

    style: remove unnecessay spaces

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,23 @@
 /**
  * @namespace Flicking.Plugins
  */
- import Parallax from "./Parallax";
- import Fade from "./Fade";
- import AutoPlay from "./AutoPlay";
- import Arrow from "./Arrow";
- import Sync from "./Sync";
- import Gallery from "./Gallery";
+import Parallax from "./Parallax";
+import Fade from "./Fade";
+import AutoPlay from "./AutoPlay";
+import Arrow from "./Arrow";
+import Sync from "./Sync";
+import Gallery from "./Gallery";
  
- export * from "./pagination";
- 
- export {
-   Parallax,
-   Fade,
-   AutoPlay,
-   Arrow,
-   Sync,
-   Gallery
- };
- 
- export * from "./const";
+export * from "./pagination";
+
+export {
+  Parallax,
+  Fade,
+  AutoPlay,
+  Arrow,
+  Sync,
+  Gallery
+};
+
+export * from "./const";
  
