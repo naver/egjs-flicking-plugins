6b36c7b81345edc41419e8b7ecfb382951257370
seonghyun.ahn <seonghyun.ahn@navercorp.com>
2021-07-14 17:16:02 +0900

    test: add unit test for sync options

--- test/unit/Sync.spec.ts
+++ test/unit/Sync.spec.ts
@@ -1,7 +1,8 @@
+import { ComponentEvent } from "@egjs/component";
 import Flicking from "@egjs/flicking";
 
 import Sync from "../../src/Sync";
-import { sandbox, cleanup, waitEvent } from "../unit/utils";
+import { sandbox, cleanup, waitEvent, tick } from "../unit/utils";
 
 describe("Sync", () => {
   let flicking0: Flicking;
@@ -25,7 +26,6 @@ describe("Sync", () => {
     wrapper1.className = "flicking-viewport";
     wrapper1.innerHTML = `
       <div class="flicking-camera">
-        <div style="width: 200px; height: 200px;"><p></p></div>
         <div style="width: 200px; height: 200px;"><p></p></div>
         <div style="width: 200px; height: 200px;"><p></p></div>
         <div style="width: 200px; height: 200px;"><p></p></div>
@@ -85,5 +85,79 @@ describe("Sync", () => {
 
     // Then
     expect(flicking0.camera.position).not.to.equal(100);
+  });
+
+  it("active panel should have active class", async () => {
+    // Given
+    flicking0.addPlugins(new Sync({
+      type: "index",
+      synchronizedFlickingOptions: [
+        {
+          flicking: flicking0,
+          isSlidable: true
+        },
+        {
+          flicking: flicking1,
+          activeClass: "flicking-thumbnail-active"
+        }
+      ]
+    }));
+    await waitEvent(flicking0, "ready");
+
+    // When
+    void flicking0.moveTo(1, 0);
+    tick(100);
+
+    // Then
+    expect(flicking1.getPanel(0).element.className.includes("flicking-thumbnail-active")).equal(false);
+    expect(flicking1.getPanel(3).element.className.includes("flicking-thumbnail-active")).equal(true);
+  });
+
+  it("slidable flicking should move other flickings", async () => {
+    // Given
+    flicking0.addPlugins(new Sync({
+      type: "index",
+      synchronizedFlickingOptions: [
+        {
+          flicking: flicking0,
+          isSlidable: true
+        },
+        {
+          flicking: flicking1
+        }
+      ]
+    }));
+    await waitEvent(flicking0, "ready");
+
+    // When
+    void flicking0.moveTo(1, 0);
+    tick(100);
+
+    // Then
+    expect(flicking1.camera.position).not.to.equal(100);
+  });
+
+  it("clickable flicking should move other flickings", async () => {
+    // Given
+    flicking0.addPlugins(new Sync({
+      type: "index",
+      synchronizedFlickingOptions: [
+        {
+          flicking: flicking0,
+          isClickable: true
+        },
+        {
+          flicking: flicking1
+        }
+      ]
+    }));
+    await waitEvent(flicking0, "ready");
+
+    // When
+    flicking0.trigger(new ComponentEvent("select", { index: 2, panel: flicking0.getPanel(2), direction: null }));
+    tick(100);
+
+    // Then
+    expect(flicking1.camera.position).not.to.equal(100);
   });
 });
