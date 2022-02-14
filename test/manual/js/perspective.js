677aa5401fe042f46869d300d3c6f3ffe88e35e6
gusrb3164 <gusrb3164@gmail.com>
2022-02-14 20:57:43 +0900

    refactor: remove empty object

--- test/manual/js/perspective.js
+++ test/manual/js/perspective.js
@@ -1,6 +1,6 @@
 const flicking = new Flicking("#perspective");
 
 // Perspective
-const perspective = new Flicking.Plugins.Perspective({});
+const perspective = new Flicking.Plugins.Perspective();
 flicking.addPlugins(perspective)
 
